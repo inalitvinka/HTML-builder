8962797efeed12c924622adf912b447712eda883
Ina Litvinka <inlibitum@gmail.com>
2025-01-20 20:36:10 +0100

    fix: replace appendFile with writeStream to merge files

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,19 +1,26 @@
+const fs = require('fs');
 const path = require('path');
-const { readdir, readFile, appendFile, stat, mkdir, rm, copyFile } = require('node:fs/promises');
+const { readdir, readFile, writeFile, appendFile, stat, mkdir } = require('node:fs/promises');
 const { stdout } = require('process');
 
 const rootDirPath = path.join(__dirname, 'styles');
 const destDirPath = path.join(__dirname, 'project-dist', 'bundle.css');
 const requiredExtantion = '.css';
+const stylesToMerge = [];
 
 async function mergeStyles() {
   try {
+    const writeStream = fs.createWriteStream(destDirPath, 'utf-8');
     const allFiles = await readdir(rootDirPath, {withFileTypes: true});
     const cssfiles = allFiles.filter(file => file.isFile() && file.name.slice(-4) === requiredExtantion);
     for (const file of cssfiles) {
-      const filesContent = await readFile(path.join(rootDirPath, file.name), 'utf-8');
-      await appendFile(destDirPath, filesContent);
+      const stylesContent = await readFile(path.join(rootDirPath, file.name), 'utf-8');
+      stylesToMerge.push(stylesContent);
+      // writeStream.write(`${stylesContent}\n`);
     }
+    writeStream.write(`${stylesToMerge.join('\n')}\n`);
+    // await writeFile(path.join(destDirPath, 'bundle.css'), stylesToMerge.join(' '),
+    // );
     stdout.write('Bundle is created');
   } catch (error) {
     console.error(error);
